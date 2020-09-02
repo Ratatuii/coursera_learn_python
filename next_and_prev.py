f96a09fc1db127eed6019b337f38ecef78cb565b
Andrei Karpenkin <andreikarpenkin123@gmail.com>
2020-09-02 20:20:05 +0300

    add some tasks

---
+++ next_and_prev.py
@@ -0,0 +1,6 @@
+number = int(input())
+next_num = number + 1
+prev_num = number - 1
+print('The next number for the number', number, 'is', next_num, end='.')
+print()
+print('The previous number for the number', number, 'is', prev_num, end='.')
