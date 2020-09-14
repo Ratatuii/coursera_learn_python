968f473ffa33d8bdbb9eb1461e19edf31afd71ef
Andrei Karpenkin <andreikarpenkin123@gmail.com>
2020-09-14 13:02:38 +0300

    add tasks chocolate

--- bool_and_if/chocolate.py
+++ bool_and_if/chocolate.py
@@ -2,11 +2,9 @@ n = int(input())
 m = int(input())
 k = int(input())
 
-chocolate = n * m
-tmp = chocolate % k
-if tmp == n or tmp == m:
-    print('YES')
-elif n == m and chocolate % n == 0 and k % 2 == 0 and chocolate - k > 0:
-    print('YES')
+tmp1 = k / m
+tmp2 = k / n
+if ((tmp1 <= n and k % n == 0) or (tmp2 <= m and k % m == 0)):
+    print("YES")
 else:
-    print('NO')
+    print("NO")
