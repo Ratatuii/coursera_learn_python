8ea07f8923aaff506c5a8199c138788e241fc40d
iRatatuii <andreikarpenkin123@gmail.com>
2020-09-21 13:30:02 +0300

    add task

---
+++ bool_and_if/Prisoner.py
@@ -0,0 +1,9 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+if e * d >= a * c or e * d >= b * c:
+    print('YES')
+else:
+    print('NO')
---
+++ bool_and_if/coincides.py
@@ -0,0 +1,9 @@
+a = int(input())
+b = int(input())
+c = int(input())
+if a == b and a == c:
+    print(3)
+elif a == b and a != c or a == c and a != b or b == c and a != c:
+    print(2)
+else:
+    print(0)
